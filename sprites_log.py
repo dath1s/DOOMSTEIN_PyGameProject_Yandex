1806d49dabd4f851f9d1a1541c5025a90b1fc98f
dath1s <kovtunov.04@gmail.com>
2022-01-16 20:21:41 +0900

    v1.8.3 Наводили красоту

--- sprites_log.py
+++ sprites_log.py
@@ -6,39 +6,62 @@ from collections import deque
 class Sprites:
     def __init__(self):
         self.sprite_params = {
-            'sprite_barrel':{
+            'sprite_barrel': {
                 'sprite': pg.image.load('sprites/barrel/base/0.png').convert_alpha(),
                 'viewing_angles': None,
                 'shift': 1.8,
-                'scale': 0.4,
+                'scale': (0.4, 0.4),
                 'animation': deque([
                     pg.image.load(f'sprites/barrel/anim/{i}.png').convert_alpha() for i in range(8)
                 ]),
+                'death_animation': deque([
+                    pg.image.load(f'sprites/barrel/death/{i}.png').convert_alpha() for i in range(4)
+                ]),
+                'is_dead': None,
+                'death_shift': 2.6,
                 'animation_dist': 800,
                 'animation_speed': 10,
-                'blocked': True
+                'blocked': True,
+                'flag': 'decor',
+                'obj_action': [],
+                'side': 30
             },
             'sprite_devil': {
                 'sprite': [pg.image.load(f'sprites/devil/base/{i}.png').convert_alpha() for i in range(8)],
                 'viewing_angles': True,
                 'shift': -0.2,
-                'scale': 1.1,
-                'animation': deque(
-                    [pg.image.load(f'sprites/devil/anim/{i}.png').convert_alpha() for i in range(9)]),
+                'scale': (1.1, 1.1),
+                'animation': [],
+                'death_animation': deque([
+                    pg.image.load(f'sprites/devil/death/{i}.png').convert_alpha() for i in range(6)
+                ]),
+                'is_dead': None,
+                'death_shift': 0.6,
                 'animation_dist': 150,
                 'animation_speed': 10,
-                'blocked': True
+                'blocked': True,
+                'flag': 'npc',
+                'obj_action': deque([
+                    pg.image.load(f'sprites/devil/anim/{i}.png').convert_alpha() for i in range(9)
+                ]),
+                'side': 50
             },
             'sprite_flame': {
                 'sprite': pg.image.load('sprites/flame/base/0.png').convert_alpha(),
                 'viewing_angles': None,
                 'shift': 0.7,
-                'scale': 0.6,
+                'scale': (0.6, 0.6),
                 'animation': deque(
                     [pg.image.load(f'sprites/flame/anim/{i}.png').convert_alpha() for i in range(16)]),
+                'death_animation': [],
+                'is_dead': 'immortal',
+                'death_shift': 1.8,
                 'animation_dist': 800,
                 'animation_speed': 5,
-                'blocked': False
+                'blocked': False,
+                'flag': 'decor',
+                'obj_action': [],
+                'side': 30
             }
         }
         # self.sprite_types = {
@@ -58,22 +81,45 @@ class Sprites:
     def sprite_shot(self):
         return min([obj.is_on_fireway for obj in self.obj_list], default=(float('inf'), 0))
 
+
 class SpriteObj:
     def __init__(self, params, pos):
-        self.obj = params['sprite']
+        self.obj = params['sprite'].copy()
         self.viewing_angles = params['viewing_angles']
-        self.x, self.y = pos[0] * TILE_WIDTH, pos[1] * TILE_WIDTH
         self.shift = params['shift']
         self.scale = params['scale']
-        self.animation = params['animation']
+        self.animation = params['animation'].copy()
+
+        self.death_animation = params['death_animation'].copy()
+        self.is_dead = params['is_dead']
+        self.dead_shift = params['death_shift']
+
         self.animation_dist = params['animation_dist']
         self.animation_speed = params['animation_speed']
-        self.animation_counter = 0
+
         self.blocked = params['blocked']
-        self.side = 30
+        self.flag = params['flag']
+        self.obj_action = params['obj_action'].copy()
+        self.x, self.y = pos[0] * TILE_WIDTH, pos[1] * TILE_WIDTH
+        self.side = params['side']
+
+        self.dead_animation_count = 0
+        self.animation_counter = 0
+
+        self.npc_action_trigger = False
+        self.door_open_trigger = False
+
+        self.door_prev_pos = self.y if self.flag == 'door_h' else self.x
+
+        self.delete = False
 
         if self.viewing_angles:
-            self.sprite_angles = [frozenset(range(i, i + 45)) for i in range(0, 360, 45)]
+            if len(self.obj) == 8:
+                self.sprite_angles = [frozenset(range(338, 361)) | frozenset(range(0, 23))] + \
+                                     [frozenset(range(i, i + 45)) for i in range(23, 338, 45)]
+            else:
+                self.sprite_angles = [frozenset(range(348, 361)) | frozenset(range(0, 11))] + \
+                                     [frozenset(range(i, i + 23)) for i in range(11, 348, 23)]
             self.sprite_positions = {angle: pos for angle, pos in zip(self.sprite_angles, self.obj)}
 
     @property
@@ -94,6 +140,7 @@ class SpriteObj:
         gamma = self.theta - player.angle
         if dx > 0 and 180 <= math.degrees(player.angle) <= 360 or dx < 0 and dy < 0:
             gamma += DOUBLE_PI
+        self.theta -= 1.4 * gamma
 
         dt_rays = int(gamma / dt_ANGLE)
         self.cur_ray = Center_RAY + dt_rays
@@ -102,33 +149,69 @@ class SpriteObj:
         fake_ray = self.cur_ray + NONE_VISIABLE_RAYS
 
         if 0 <= fake_ray <= FAKE_RAYS_RANGE and self.dist_to_sprite > 30:
-            self.proj_height = min(int(PROJECTION_C / self.dist_to_sprite * self.scale), DOUBLE_HEIGHT)
-            half_projection_height = self.proj_height // 2
-            shift = half_projection_height * self.shift
+            self.proj_height = min(int(PROJECTION_C / self.dist_to_sprite), DOUBLE_HEIGHT)
+            sprite_width = int(self.proj_height * self.scale[0])
+            sprite_height = int(self.proj_height * self.scale[1])
+            half_sprite_width = sprite_width // 2
+            half_sprite_height = sprite_height // 2
+            shift = half_sprite_height * self.shift
 
-            if self.viewing_angles:
-                if self.theta < 0:
-                    self.theta += DOUBLE_PI
-                self.theta = 360 - int(math.degrees(self.theta))
-
-                for angles in self.sprite_angles:
-                    if self.theta in angles:
-                        self.obj = self.sprite_positions[angles]
-
-            # Анимация спрайтов
-            sprite_obj = self.obj
-            if self.animation and self.dist_to_sprite < self.animation_dist:
-                sprite_obj = self.animation[0]
-                if self.animation_counter < self.animation_speed:
-                    self.animation_counter += 1
-                else:
-                    self.animation.rotate()
-                    self.animation_counter = 0
+            if self.is_dead and self.is_dead != 'immortal':
+                sprite_object = self.death_animation()
+                shift = half_sprite_height * self.dead_shift
+                sprite_height = int(sprite_height / 1.3)
+            elif self.npc_action_trigger:
+                sprite_object = self.npc_action()
+            else:
+                self.obj = self.visible_sprite()
+                sprite_object = self.sprite_animation()
 
             # Проекция спрайта
-            sprite_position = (self.cur_ray * SCALE - half_projection_height, HALF_HEIGHT - half_projection_height + shift)
-            sprite = pg.transform.scale(sprite_obj, (self.proj_height, self.proj_height))
+            sprite_position = (
+            self.cur_ray * SCALE - half_sprite_width, HALF_HEIGHT - half_sprite_height + shift)
+            sprite = pg.transform.scale(sprite_object, (sprite_width, sprite_height))
 
             return (self.dist_to_sprite, sprite, sprite_position, None)
         else:
             return (False,)
+
+    def sprite_animation(self):
+        if self.animation and self.dist_to_sprite < self.animation_dist:
+            sprite_obj = self.animation[0]
+            if self.animation_counter < self.animation_speed:
+                self.animation_counter += 1
+            else:
+                self.animation.rotate()
+                self.animation_counter = 0
+            return  sprite_obj
+        return self.obj
+
+    def visible_sprite(self):
+        if self.viewing_angles:
+            if self.theta < 0:
+                self.theta += DOUBLE_PI
+            self.theta = 360 - int(math.degrees(self.theta))
+
+            for angles in self.sprite_angles:
+                if self.theta in angles:
+                    return self.sprite_positions[angles]
+        return self.obj
+
+    def death_animation(self):
+        if len(self.death_animation):
+            if self.dead_animation_count < self.animation_speed:
+                self.dead_sprite = self.death_animation[0]
+                self.dead_animation_count += 1
+            else:
+                self.dead_sprite = self.death_animation.popleft()
+                self.dead_animation_count = 0
+        return self.dead_sprite
+
+    def npc_action(self):
+        sprite_object = self.obj_action[0]
+        if self.animation_count < self.animation_speed:
+            self.animation_count += 1
+        else:
+            self.obj_action.rotate()
+            self.animation_count = 0
+        return sprite_object
